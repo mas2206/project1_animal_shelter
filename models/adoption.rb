2ad600de394c9393950e8ce2659174bc6eb68b95
Michael Shennan <m.shennan22@gmail.com>
2017-03-31 21:54:46 +0100

    added CRUD methods to all classes

--- models/adoption.rb
+++ models/adoption.rb
@@ -4,8 +4,36 @@ class Adoption
 
   def initialize(options)
     @id = options['id'].to_i
-    @animal_id = options['animal_id']
-    @owner_id = options['owner_id']
+    @animal_id = options['animal_id'].to_i
+    @owner_id = options['owner_id'].to_i
+  end
+
+  def save()
+    sql = "INSERT INTO adoptions (animal_id, owner_id) VALUES (#{@animal_id}, #{@owner_id}) RETURNING *;"
+    results = SqlRunner.run(sql)
+    @id = results.first['id'].to_i
+  end
+
+  def delete()
+    sql = "DELETE FROM adoptions WHERE id = #{@id};"
+    SqlRunner.run(sql)
+  end
+
+  def self.all()
+    sql = "SELECT * FROM adoptions;"
+    results = SqlRunner.run(sql)
+    return results.map {|adoptions_hash| Adoption.new(adoptions_hash)}
+  end
+
+  def self.find(id)
+    sql = "SELECT FROM adoptions WHERE id = #{@id};"
+    results = SqlRunner.run(sql)
+    return Adoption.new(results.first)
+  end
+
+  def delete_all()
+    sql = "DELETE FROM adoptions;"
+    SqlRunner.run(sql)
   end
 
 end
--- models/owner.rb
+++ models/owner.rb
@@ -7,4 +7,32 @@ class Owner
     @name = options['name']
   end
 
+  def save()
+    sql = "INSERT INTO owners (name) VALUES ('#{@name}') RETURNING *;"
+    results = SqlRunner.run(sql)
+    @id = results.first['id'].to_i
+  end
+
+  def delete()
+    sql = "DELETE FROM owners WHERE id = #{@id};"
+    SqlRunner.run(sql)
+  end
+
+  def self.all()
+    sql = "SELECT * FROM owners;"
+    results = SqlRunner.run(sql)
+    return results.map {|owners_hash| Owner.new(owners_hash)}
+  end
+
+  def self.find(id)
+    sql = "SELECT * FROM owners WHERE id = #{@id};"
+    results = SqlRunner.run(sql)
+    return Owner.new(results.first)
+  end
+
+  def delete_all()
+    sql = "DELETE FROM owners;"
+    SqlRunner.run(sql)
+  end
+
 end
