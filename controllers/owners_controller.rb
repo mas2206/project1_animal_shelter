9578dab39f3f4cb0ed7a3ca2ae30e94f43c1d46c
Michael Shennan <m.shennan22@gmail.com>
2017-04-04 10:45:27 +0100

    submit new owner working

--- controllers/owners_controller.rb
+++ controllers/owners_controller.rb
@@ -6,3 +6,13 @@ get '/owners' do
   @owners = Owner.all()
   erb(:'owners/index')
 end
+
+get '/owners/new' do
+  erb(:'owners/new')
+end
+
+post '/owners' do
+  owner = Owner.new(params)
+  owner.save()
+  redirect to ('/owners')
+end
